de3cfddf8b5337f566d6e5bfd46d1ecbd35b4dc2
Brett Branscombe <103083140+oascout25@users.noreply.github.com>
2023-04-23 21:57:53 -0400

    Create entrypoint.sh
    Script to generate .goracle from environment variables in docker-compose.yml. Then waits for Algorand node to be caught-up before launching the Goracle node.

---
+++ entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Shut down existing Goracle Container
+if [ "$(docker ps -q -f name=goracle-nr)" ]; then
+    docker stop goracle-nr
+fi
+
+# Extract the algod.token
+ALGOD_TOKEN=$(cat /algod/data/algod.token)
+
+# Create the .goracle file with the extracted token, mnemonic, and the user's main Algorand public address
+cat > "/root/.goracle" << EOL
+{
+  "blockchain": {
+    "server": "http://$ALGORAND_NODE_IP:8080",
+    "token": "$ALGOD_TOKEN",
+    "mnemonic": "$PARTICIPATION_SEED_PHRASE",
+    "mainAddr": "$MAIN_ADDRESS_PUBLIC_KEY"
+  }
+}
+EOL
+
+# Wait for the Algorand node to catch up
+while true; do
+  RESPONSE=$(curl -s -H "X-Algo-API-Token: $ALGOD_TOKEN" "http://$ALGORAND_NODE_IP:8080/v2/status")
+
+  LAST_ROUND=$(echo "$RESPONSE" | jq '.["last-round"]')
+  CATCHUP_TIME=$(echo "$RESPONSE" | jq '.["catchup-time"]')
+  catchup_seconds=$((CATCHUP_TIME / 1000000000))
+  hours=$((catchup_seconds / 3600))
+  minutes=$(( (catchup_seconds % 3600) / 60 ))
+  seconds=$((catchup_seconds % 60))
+  formatted_catchup_time=$(printf "%02d:%02d:%02d" $hours $minutes $seconds)
+
+  if [ "$CATCHUP_TIME" -eq 0 ]; then
+    echo "Node is caught up. Last round: $LAST_ROUND"
+    break
+  else
+    echo "Node is still catching up. Last round: $LAST_ROUND, Catchup time: $formatted_catchup_time"
+    sleep 60
+  fi
+done
+
+# Start Goracle Node Runner
+/usr/bin/node --trace-uncaught ./node_runner.js
